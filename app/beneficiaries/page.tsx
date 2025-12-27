7bc87d935dec41e7b2027b866b097145ab02c9eb
Harsh Kumar <apkharsh@gmail.com>
2025-12-28 00:17:40 +0530

    removed unsuppported prop

--- app/beneficiaries/page.tsx
+++ app/beneficiaries/page.tsx
@@ -155,7 +155,7 @@ export default function BeneficiariesPage() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar user={user} currentPage="beneficiaries" />
+      <Navbar currentPage="beneficiaries" />
 
       <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         {/* Header with Add Button */}
